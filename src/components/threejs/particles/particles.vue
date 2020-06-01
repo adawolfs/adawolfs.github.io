a37b52c71f0606b4e3f66711895e3235d6e297f4
adawolfs <adawolfs@gmail.com>
2020-05-31 21:57:39 -0600

    set as const

--- src/components/threejs/particles/particles.vue
+++ src/components/threejs/particles/particles.vue
@@ -7,37 +7,37 @@
 </template>
 
 <script>
-
-
 export default {
-  data : function() {
+  data: function() {
     return {
       container: {},
       camera: {},
-      scene: {}, 
+      scene: {},
       renderer: {},
       spheres: [],
-      lines : [],
-      mouseY : 0,
-      mouseX : 0,
-      totalParticles : 700,
-      minSpehereDist : 10,
-      windowHalfX : window.innerWidth / 2,
-      windowHalfY : window.innerHeight / 2,
-    }
+      lines: [],
+      mouseY: 0,
+      mouseX: 0,
+      totalParticles: 700,
+      minSpehereDist: 10,
+      windowHalfX: window.innerWidth / 2,
+      windowHalfY: window.innerHeight / 2
+    };
   },
-  mounted(){
-    let recaptchaScript = document.createElement('script')
-    recaptchaScript.setAttribute('src', 'http://localhost:8080/js/particles.js')
-    recaptchaScript.setAttribute('type', 'module')
-    document.head.appendChild(recaptchaScript)
-    
+  mounted() {
+    const recaptchaScript = document.createElement("script");
+    recaptchaScript.setAttribute(
+      "src",
+      "http://localhost:8080/js/particles.js"
+    );
+    recaptchaScript.setAttribute("type", "module");
+    document.head.appendChild(recaptchaScript);
   }
-}
+};
 </script>
 
 <style>
- #threejs-particles {
-   background-color: black;
- }
+#threejs-particles {
+  background-color: black;
+}
 </style>
